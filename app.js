01cd13087e64370c3a45773cd14ce0e8a83f7fec
Ahmad Fakhry <afakhry98@gmail.com>
2019-12-18 10:36:11 +0300

    Added Angular

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const apiController = require("./controllers/apiController");
 
 const port = process.env.PORT | 3000;
 
-app.use("/assets", express.static(__dirname + "/public"));
+app.use("/", express.static(__dirname + "/public/src"));
 
 app.set("view engine", "ejs");
 
