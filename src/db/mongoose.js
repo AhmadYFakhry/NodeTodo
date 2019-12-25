5f73bf6fd63f7ab72b135e91ae93e88ec42c6924
Ahmad Fakhry <afakhry98@gmail.com>
2019-12-25 17:37:38 +0300

    test

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 // Connect to the db
+console.log(process.env.MONGODB_URL)
 mongoose.connect(process.env.MONGODB_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
