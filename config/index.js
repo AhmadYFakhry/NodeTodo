b66ee97b9b57b475d9f07cf9b156b93950f94ff6
Ahmad Fakhry <afakhry98@gmail.com>
2019-12-25 17:00:03 +0300

    Added email support

--- config/index.js
+++
@@ -1,8 +0,0 @@
-const configValues = require('./config.json');
-
-module.exports = {
-
-    getDbConnectionString: function () {
-        return `mongodb+srv://${configValues.uname}:${configValues.pswd}@nodetodosample-4gyoc.mongodb.net/test?retryWrites=true&w=majority`;
-    }
-}
--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,8 +1,7 @@
 const mongoose = require("mongoose");
 // Connect to the db
-mongoose.connect("mongodb://127.0.0.1:27017/task-manager-api", {
+mongoose.connect(process.env.MONGODB_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true
 });
-
