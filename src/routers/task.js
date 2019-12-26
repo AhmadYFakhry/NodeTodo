cd5763613819d70b74aa14444fe3443c393e33b6
Ahmad Fakhry <afakhry98@gmail.com>
2019-12-26 14:46:03 +0300

    Unit tests completed, some last changes

--- src/routers/task.js
+++ src/routers/task.js
@@ -12,7 +12,7 @@ router.post('/tasks', auth, async (req, res) => {
     })
     try {
         await task.save();
-        res.status(200).send(task);
+        res.status(201).send(task);
     } catch (e) {
         res.status(400).send(e);
     }
@@ -63,11 +63,12 @@ router.get('/tasks/:id', auth, async (req, res) => {
 router.delete('/tasks/:id', auth, async (req, res) => {
     const _id = req.params.id;
     try {
-        const task = await Task.findOneAndDelete({
-            _id,
-            owner: req.user._id
-        });
+        const task = await Task.findById(_id)
+        if (task.author !== req.user._id) return res.status(401).send();
         if (!task) return res.status(404).send();
+        await Task.deleteOne({
+            _id
+        })
         res.send();
     } catch (e) {
         res.status(500).send(e);
